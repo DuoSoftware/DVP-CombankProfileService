a7ebc6bc36b373dc74bd6a2f19bb440727c61131
Nimeshka Srimal <nimeshka@gmail.com>
2019-01-25 11:12:04 +0530

    Added not implemented message on mongo handler

--- db_mongo_handler.js
+++ db_mongo_handler.js
@@ -25,7 +25,7 @@ module.exports.getProfileData = function (req, res) {
         res.end(jsonString);
     })
     .catch(function(err) {
-        var jsonString = messageFormatter.FormatMessage(err, "ERROR", false, emptyArr);
+        var jsonString = messageFormatter.FormatMessage(err, "ERROR", false, []);
         logger.debug('[DVP-CDRProcessor.GetExternalUserFacility] - [%s] - API RESPONSE : %s', reqId, jsonString);
         res.end(jsonString);
     });
@@ -33,102 +33,25 @@ module.exports.getProfileData = function (req, res) {
 };
 
 module.exports.getImportantData = function (req, res) {
-    logger.info('getImportantData', req.params.Reference);
+    logger.info('getImportantData');
+    var jsonString = messageFormatter.FormatMessage(undefined, "Method Not Implemented", false, null);
+    res.end(jsonString);
 };
 
 module.exports.getProfileDataFacetone = function (req, res) {
-    logger.info('getProfileDataFacetone', req.params.Reference);
+    logger.info('getProfileDataFacetone');
+    var jsonString = messageFormatter.FormatMessage(undefined, "Method Not Implemented", false, null);
+    res.end(jsonString);
 };
 
 module.exports.profileSearch = function (req, res) {
-    logger.info('profileSearch', req.params.Reference);
+    logger.info('profileSearch');
+    var jsonString = messageFormatter.FormatMessage(undefined, "Method Not Implemented", false, null);
+    res.end(jsonString);
 };
 
 module.exports.saveImageFile = function (req, res) {
-    pool.open(cn, function (err, dbCon) {
-        if (err) {
-            res.status(522);
-            res.end(err.message);
-        }
-        else {
-
-            /*File file = new File("picture.gif");
-            InputStream istr = new FileInputStream("picture.gif");
-            PreparedStatement pstmt = conn.prepareStatement
-            ("INSERT INTO webimage (ident, fmt, data) VALUES (?, ?, ?)");
-            pstmt.setString(1, "Picture");
-            pstmt.setString(2, "GIF");
-            pstmt.setBinaryStream(3, istr, (int)file.length());
-            pstmt.execute();*/
-
-           /* var pstmt = dbCon.prepare("INSERT INTO WARUNA.EMPLOYEE (EMPNO,FIRSTNME,PICTURE) VALUES (?, ?, ?);",function (err, data) {
-
-            });
-            pstmt.setString(1, "Picture");
-            pstmt.setString(2, "GIF");
-            pstmt.setBinaryStrea (3, istr, (int)file.length());
-            pstmt.execute();*/
-
-            fs.readFile("E:\\call_now_button.png", function (status, fd) {
-                if (status) {
-                    console.log(status.message);
-                    return;
-                }
-                var base64Image = fd.toString('base64');
-                var buffer = new Buffer(base64Image, 'base64');
-
-                // var query ="INSERT INTO WARUNA.EMPLOYEE (EMPNO,FIRSTNME,PICTURE) VALUES ('img', 'test','" + buffer + "' );";
-                //var query ="INSERT INTO WARUNA.TAB1 (COL1,COL2) VALUES (?,?);";
-                dbCon.prepare("INSERT INTO WARUNA.TAB1 (COL1,COL2) VALUES (?,?)", function (err, stmt) {
-                    if (err) {
-                        //could not prepare for some reason
-                        console.log(err);
-                        return conn.closeSync();
-                    }
-
-
-                    //Bind and Execute the statment asynchronously
-                    stmt.execute([1,buffer], function (err, result) {
-                        if( err ) console.log(err);
-                        else result.closeSync();
-
-                        //Close the connection
-                        conn.close(function(err){});
-                    });
-                });
-
-                /*dbCon.query(query, function (er, da) {
-                    if (er)throw er;
-                });*/
-            });
-
-            /*var query = util.format(config.DBTWO.ImportantDataSp, req.params.Reference);
-            dbCon.query(query, function (err, data) {
-                if (err) {
-                    res.status(444);
-                    res.end(err.message);
-                }
-                else {
-                    var newobj = {};
-                    if (data && data.length) {
-                        var key, keys = Object.keys(data[0]);
-                        var n = keys.length;
-                        while (n--) {
-                            key = keys[n];
-                            newobj[key.toLowerCase()] = data[0][key];
-                        }
-                    }
-
-                    logger.info("getImportantData  - Data found  - [%s]", data);
-                    res.end(JSON.stringify(newobj));
-                }
-                dbCon.close(function () {
-                    logger.info("Connection Close.");
-                })
-            });*/
-        }
-
-    });
-
-
+    logger.info('saveImageFile');
+    var jsonString = messageFormatter.FormatMessage(undefined, "Method Not Implemented", false, null);
+    res.end(jsonString);
 };
